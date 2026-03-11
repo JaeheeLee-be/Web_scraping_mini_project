4f11ac24a1b232074a41cd338212b3d086934993
JaeheeLee-be <clodusky1@gmail.com>
2026-03-11 16:55:53 +0900

    orm 변경 및 api.diary 추가

--- fastapi_mini_project/app/api/v1/diary.py
+++ fastapi_mini_project/app/api/v1/diary.py
@@ -1,30 +1,32 @@
 from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
-from app.db.session import get_db
-from schemas.diary import DiaryCreate, DiaryUpdate
+from core.security import get_current_user
+from schemas.diary import DiaryCreate, DiaryUpdate, DiaryResponse
 from services.diary_service import diary_service
+
 
 router = APIRouter(prefix="/diaries", tags=["Diary"])
 
 
 @router.post("", response_model=DiaryCreate, status_code=status.HTTP_201_CREATED)
-def create_diary(
+async def create_diary(
         data: DiaryCreate,
-        db: Session = Depends(get_db),
         current_user = Depends(get_current_user)
 ):
-    return diary_service.create_diary(db, current_user.id, data)
+    return await diary_service.create_diary(current_user.id, data)
 
 
 @router.patch("/{diary_id}", response_model=DiaryResponse)
-def update_diary(
+async def update_diary(
         diary_id: int,
         data: DiaryUpdate,
-        db: Session = Depends(get_db),
         current_user = Depends(get_current_user)
 ):
-    return diary_service.update_diary(db, diary_id, current_user.id, data)
+    return await diary_service.update_diary(diary_id, current_user.id, data)
 
 
-@router.delete("/{diary_id}")
-
+@router.delete("/{diary_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_diary(
+        diary_id: int,
+        current_user = Depends(get_current_user)
+):
+    await diary_service.delete_diary(diary_id, current_user.id)
--- fastapi_mini_project/app/services/diary_service.py
+++ fastapi_mini_project/app/services/diary_service.py
@@ -1,22 +1,21 @@
-from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from repositories.diary_repo import diary_repository
 from schemas.diary import DiaryCreate, DiaryUpdate
 
 
 class DiaryService:
-    def create_diary(self, db: Session, user_id: int, data: DiaryCreate):
-        return diary_repository.create(db, user_id, data)
+    async def create_diary(self, user_id: int, data: DiaryCreate):
+        return await diary_repository.create(user_id, data)
 
 
-    def update_diary(self, db: Session, diary_id: int, user_id: int, data: DiaryUpdate):
-        diary = self.get_diary(db, diary_id, user_id)
-        return diary_repository.update(db, diary, data)
+    async def update_diary(self, diary_id: int, user_id: int, data: DiaryUpdate):
+        diary = await self.get_diary(diary_id, user_id)
+        return await diary_repository.update(diary, data)
 
 
-    def delete_diary(self, db: Session, diary_id: int, user_id: int):
-        diary = self.get_diary(db, diary_id, user_id)
-        diary_repository.delete(db, diary)
+    async def delete_diary(self, diary_id: int, user_id: int):
+        diary = await self.get_diary(diary_id, user_id)
+        await diary_repository.delete(diary)
 
 
 diary_service = DiaryService()
