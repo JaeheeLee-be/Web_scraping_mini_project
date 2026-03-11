b1f68b4199da1ace1e9050c94e83901d010d22ca
kochanyeol <cksdufqqpu@gmail.com>
2026-03-11 18:48:19 +0900

    수정

--- fastapi_mini_project/app/core/security.py
+++ fastapi_mini_project/app/core/security.py
@@ -12,8 +12,8 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from core.config import settings
-from models.user import User
+from app.core.config import settings
+from app.models.user import User
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
