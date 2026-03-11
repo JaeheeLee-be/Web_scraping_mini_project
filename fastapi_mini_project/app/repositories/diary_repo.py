c9a19737755f5a4d63ae8c134138cf9d532855be
JaeheeLee-be <clodusky1@gmail.com>
2026-03-11 15:06:30 +0900

    CRUD 재작업

---
+++ fastapi_mini_project/app/repositories/diary_repo.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from models.diary import Diary
+from schemas.diary import DiaryCreate, DiaryUpdate
+from typing import Optional
+
+
+class DiaryRepo:
+    def create(self, db: Session, user_id: int, data: DiaryCreate) -> Diary:
+        diary = Diary(
+            user_id=user_id,
+            title=data.title,
+            content=data.content
+        )
+
+        db.add(diary)
+        db.commit()
+        db.refresh(diary)
+        return diary
+
+
+    def update(self, db: Session, diary: Diary, data: DiaryUpdate) -> Diary:
+        update_data = data.model_dump(exclude_unset=True)
+        for field, value in update_data.items():
+            setattr(diary, field, value)
+
+        db.commit()
+        db.refresh(diary)
+        return diary
+
+
+    def delete(self, db: Session, diary: Diary):
