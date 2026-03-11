1ac21ad838f1a9d9441cc02a312777a7100564e3
kochanyeol <cksdufqqpu@gmail.com>
2026-03-11 16:05:51 +0900

    수정

--- fastapi_mini_project/app/core/security.py
+++ fastapi_mini_project/app/core/security.py
@@ -6,16 +6,17 @@
 # 보안 강화
 # 토큰을 헤더에 담으면 서버가 HttpOnly 쿠키나 보안 조치와 함께 보호가 가능합니다.
 # 또한, CSRF 방지 및 토큰을 URL에 포함할 경우 북마크, 공유, 보안 문제가 발생합니다
-# 출처: https://kyung-a.tistory.com/64#왜_토큰을_HTTP_header에_담아야_하는가? [개발로그:티스토리]
-import os
-from datetime import datetime, timedelta, timezone
 
+from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.util import deprecated
+from app.core.config import settings
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 
 
 def get_password_hash(password: str) -> str:
@@ -23,13 +24,6 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
-
-# 예시
-# SECRET_KEY = "ozbe17_super_long_and_complicate_secret_key"
-# ALGORITHM = "HS256"
-# ACCESS_TOKEN_EXPIRE_MINS = 15
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
@@ -39,8 +33,16 @@ def create_access_token(data: dict) -> str:
 
 def create_refresh_token(data: dict) -> str:
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+    expire = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
-# rebase 내 분기 1 팀장님 pr 2 내 기능 브런치 = 1 로컬의 develop == header
+def decode_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithme=[settings.ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="유효하지 않는 토큰입니다.")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = decode_token(token)
