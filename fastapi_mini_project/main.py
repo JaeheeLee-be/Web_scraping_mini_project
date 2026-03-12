e292cdbf8ae4aaf059239e2dbc3726339f325165
JaeheeLee-be <clodusky1@gmail.com>
2026-03-12 19:20:11 +0900

    main.py 수정중

--- fastapi_mini_project/main.py
+++ fastapi_mini_project/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from app.core.config import settings
+from app.db.database import TORTOISE_ORM
+from app.api.v1 import diary, auth, question, quote
+
 
 app = FastAPI()
 
+app.include_router(auth.router, prefix="/api/v1")
+app.include_router(diary.router, prefix="/api/v1")
+# app.include_router(question.router, prefix="/api/v1")
+# app.include_router(quote.router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
     return {"message": "hello"}
-
 
 @app.get("/health")
 def health():
@@ -17,8 +23,7 @@ def health():
 
 register_tortoise(
     app,
-    db_url=settings.DATABASE_URL,
-    modules={"models": ["app.models"]},
+    config=TORTOISE_ORM,
     generate_schemas=True,
     add_exception_handlers=True,
 )
