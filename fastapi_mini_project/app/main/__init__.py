911220fae3d53d639d76fd59b3205286e0a1eb42
kochanyeol <cksdufqqpu@gmail.com>
2026-03-12 15:53:06 +0900

    테스트코드 임시작성

---
+++ fastapi_mini_project/app/main/__init__.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.db.database import init_db, close_db
+from app.api.v1 import diary
+
+app = FastAPI()
+
+app.include_router(auth.router, prefix="/api/v1/auth")
+app.include_router(diary.router, prefix="/api/v1")
+
+@app.on_event("startup")
+async def startup():
+    await init_db()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await close_db()
