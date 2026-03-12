0894b7ff9815c9513920ce2ce0288d08fe599c33
kochanyeol <cksdufqqpu@gmail.com>
2026-03-12 11:15:06 +0900

    수정

--- fastapi_mini_project/app/services/auth_service.py
+++ fastapi_mini_project/app/services/auth_service.py
@@ -1,9 +1,11 @@
 from fastapi import HTTPException
-from app.core.security import get_password_hash, verify_password
+from app.core.security import get_password_hash, verify_password, create_access_token, create_refresh_token
+from app.schemas.user import CreateUser, LoginUser, TokenResponse
 from app.repositories.user_repo import get_user_email, get_user_nickname, create_user
-from app.schemas.user import CreateUser, LoginUser
 
-from fastapi_mini_project.app.core.security import verify_password
+from fastapi_mini_project.app.core.security import decode_token
+from fastapi_mini_project.app.models.user import TokenBlacklist
+from fastapi_mini_project.app.repositories.user_repo import add_token_blacklist
 
 
 async def register(user_data: CreateUser):
@@ -22,7 +24,7 @@ async def register(user_data: CreateUser):
     return await create_user(
         email=user_data.email,
         nickname=user_data.nickname,
-        hashed_password=hashed_password
+        password_hash=hashed_password
     )
 
 async def login(user_data: LoginUser):
@@ -31,4 +33,24 @@ async def login(user_data: LoginUser):
     if not response_email:
         raise HTTPException(status_code=401, detail="존재하지 않는 이메일입니다.")
 
-    hashed_password = verify_password(user_data.password)
+    if not verify_password(user_data.password, response_email.password_hash):
+        raise HTTPException(status_code=401, detail="비밀번호가 일치하지 않습니다.")
+
+    access_token = create_access_token(data={"sub": str(response_email.id)})
+    refresh_token = create_refresh_token(data={"sub": str(response_email.id)})
+
+    return TokenResponse(
+        access_token=access_token,
+        refresh_token=refresh_token,
+        token_type="bearer"
+    )
+
+async def logout(user_data: current_user):
+    payload = decode_token(token)
+    expire_at = payload.get("exp")
+
+    await add_token_blacklist(
+        token=token,
+        user_id=current_user
+
+    )
